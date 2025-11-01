0efeedce91c883a32c0ad8fd5bd793d2a67d9524
hariawannurfajri <v.hariawan.fajri@adira.co.id>
2025-11-01 19:15:05 +0700

    enhance error build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// ✅ Base path harus sesuai nama repo untuk GitHub Pages
 export default defineConfig({
+  base: '/anime-search-app/', // wajib untuk GitHub Pages
   plugins: [react()],
-  base: '/anime-search-app/', // tambahkan ini
   server: {
     port: 4000,
   },
-});
+})
