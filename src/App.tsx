5f9544e6e6093ad2333462cd4d18779d29246486
hariawannurfajri <v.hariawan.fajri@adira.co.id>
2025-11-01 18:46:05 +0700

    enhance error build

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
